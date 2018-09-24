09711d3013c3078bc479c21d7a0d841d606c5a2c
dev637 <devang713@gmail.com>
2018-09-24 10:07:43 -0400

    Add score and separate css file for status bar
    Added score for correct answers and separate stylesheet for status bar. Also increased transition duration of progress bar.

--- src/components/statusBar.js
+++ src/components/statusBar.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Progress } from 'semantic-ui-react'
+import './statusBar.css'
 
 function statusBar() {
   let {quiz, quizGuesses, quizAnswers} = this.state
   let percentComp = quiz ? parseInt( quizGuesses.length / quizAnswers.length * 100, 10): ""
-  let top = quiz ? "1rem":"-3rem";
+  let top = quiz ? "0rem":"-3rem";
   return (
-    <div className="progressBar" style={{ top: `${top}` }}>
-      <Progress percent={percentComp} progress />
+    <div className="statusBar" style={{ top: `${top}` }}>
+      <Progress percent={percentComp} className="statusBar-progress" progress />
+      <div className="statusBar-ratio">
+        <p>Score: {quizGuesses.filter(x => x).length} / {quizAnswers.length}</p>
+      </div>
     </div>
   )
 }
