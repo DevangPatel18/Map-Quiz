a786513c524f901032c3efaef1c9e7d6778cf00a
dev637 <devang713@gmail.com>
2018-10-04 15:28:15 -0400

    Revise attributeFix and remove country_data
    Revised attributeFix by iterating through corrections. Also removed country_data as click and quiz data now fetched from Rest API.

--- src/helpers/attributeFix.js
+++ src/helpers/attributeFix.js
@@ -13,7 +13,7 @@ const DataFix = (geoPath, data, capitalMarkers) => {
   countryData.find(x => x.alpha3Code === 'VGB').altSpellings = ['British Virgin Islands'];
   countryData.find(x => x.alpha3Code === 'VIR').altSpellings = ['US Virgin Islands'];
 
-  // Change display name of country to shorter variant
+  // Move original name to altSpellings
   ['VEN', 'BOL', 'GBR', 'MDA', 'MKD', 'PSE', 'SYR', 'IRN', 'PRK', 'KOR', 'LAO', 'BRN', 'COD', 'TZA', 'FSM', 'BLM', 'KNA', 'LCA', 'MAF', 'SHN', 'SPM', 'VCT', 'KOS']
     .forEach((code) => {
       const country = countryData.find(x => x.alpha3Code === code);
@@ -24,43 +24,49 @@ const DataFix = (geoPath, data, capitalMarkers) => {
       }
     });
 
-  countryData.find(x => x.alpha3Code === 'VEN').name = 'Venezuela';
-  countryData.find(x => x.alpha3Code === 'BOL').name = 'Bolivia';
-  countryData.find(x => x.alpha3Code === 'GBR').name = 'United Kingdom';
-  countryData.find(x => x.alpha3Code === 'MDA').name = 'Moldova';
-  countryData.find(x => x.alpha3Code === 'MKD').name = 'Macedonia';
-  countryData.find(x => x.alpha3Code === 'KOS').name = 'Kosovo';
-  countryData.find(x => x.alpha3Code === 'PSE').name = 'Palestine';
-  countryData.find(x => x.alpha3Code === 'SYR').name = 'Syria';
-  countryData.find(x => x.alpha3Code === 'IRN').name = 'Iran';
-  countryData.find(x => x.alpha3Code === 'PRK').name = 'North Korea';
-  countryData.find(x => x.alpha3Code === 'KOR').name = 'South Korea';
-  countryData.find(x => x.alpha3Code === 'LAO').name = 'Laos';
-  countryData.find(x => x.alpha3Code === 'BRN').name = 'Brunei';
-  countryData.find(x => x.alpha3Code === 'COD').name = 'DR Congo';
-  countryData.find(x => x.alpha3Code === 'TZA').name = 'Tanzania';
-  countryData.find(x => x.alpha3Code === 'FSM').name = 'Micronesia';
-  countryData.find(x => x.alpha3Code === 'BLM').name = 'St Barthélemy';
-  countryData.find(x => x.alpha3Code === 'KNA').name = 'St Kitts and Nevis';
-  countryData.find(x => x.alpha3Code === 'LCA').name = 'St Lucia';
-  countryData.find(x => x.alpha3Code === 'MAF').name = 'St Martin (French part)';
-  countryData.find(x => x.alpha3Code === 'SHN').name = 'St Helena, Ascension and Tristan da Cunha';
-  countryData.find(x => x.alpha3Code === 'SPM').name = 'St Pierre and Miquelon';
-  countryData.find(x => x.alpha3Code === 'VCT').name = 'St Vincent and the Grenadines';
+  const dataCorrections = [
+    // Change display name of country to shorter variant
+    { alpha: 'VEN', attribute: 'name', value: 'Venezuela' },
+    { alpha: 'BOL', attribute: 'name', value: 'Bolivia' },
+    { alpha: 'GBR', attribute: 'name', value: 'United Kingdom' },
+    { alpha: 'MDA', attribute: 'name', value: 'Moldova' },
+    { alpha: 'MKD', attribute: 'name', value: 'Macedonia' },
+    { alpha: 'KOS', attribute: 'name', value: 'Kosovo' },
+    { alpha: 'PSE', attribute: 'name', value: 'Palestine' },
+    { alpha: 'SYR', attribute: 'name', value: 'Syria' },
+    { alpha: 'IRN', attribute: 'name', value: 'Iran' },
+    { alpha: 'PRK', attribute: 'name', value: 'North Korea' },
+    { alpha: 'KOR', attribute: 'name', value: 'South Korea' },
+    { alpha: 'LAO', attribute: 'name', value: 'Laos' },
+    { alpha: 'BRN', attribute: 'name', value: 'Brunei' },
+    { alpha: 'COD', attribute: 'name', value: 'DR Congo' },
+    { alpha: 'TZA', attribute: 'name', value: 'Tanzania' },
+    { alpha: 'FSM', attribute: 'name', value: 'Micronesia' },
+    { alpha: 'BLM', attribute: 'name', value: 'St Barthélemy' },
+    { alpha: 'KNA', attribute: 'name', value: 'St Kitts and Nevis' },
+    { alpha: 'LCA', attribute: 'name', value: 'St Lucia' },
+    { alpha: 'MAF', attribute: 'name', value: 'St Martin (French part)' },
+    { alpha: 'SHN', attribute: 'name', value: 'St Helena, Ascension and Tristan da Cunha' },
+    { alpha: 'SPM', attribute: 'name', value: 'St Pierre and Miquelon' },
+    { alpha: 'VCT', attribute: 'name', value: 'St Vincent and the Grenadines' },
+    // Set null areas (data based on Wikipedia)
+    { alpha: 'PSE', attribute: 'area', value: 6220 },
+    { alpha: 'SGS', attribute: 'area', value: 3903 },
+    { alpha: 'SHN', attribute: 'area', value: 394 },
+    { alpha: 'REU', attribute: 'area', value: 2511 },
+    { alpha: 'MYT', attribute: 'area', value: 374 },
+    { alpha: 'GUF', attribute: 'area', value: 83534 },
+    { alpha: 'MTQ', attribute: 'area', value: 1128 },
+    { alpha: 'GLP', attribute: 'area', value: 1628 },
+    { alpha: 'SJM', attribute: 'area', value: 62049 },
+    // Set numericCode and alpha2Code for Kosovo
+    { alpha: 'KOS', attribute: 'numericCode', value: 999 },
+    { alpha: 'KOS', attribute: 'alpha2Code', value: 'KO' },
+  ];
 
-  // Set null areas (data based on Wikipedia)
-  countryData.find(x => x.alpha3Code === 'PSE').area = 6220;
-  countryData.find(x => x.alpha3Code === 'SGS').area = 3903;
-  countryData.find(x => x.alpha3Code === 'SHN').area = 394;
-  countryData.find(x => x.alpha3Code === 'REU').area = 2511;
-  countryData.find(x => x.alpha3Code === 'MYT').area = 374;
-  countryData.find(x => x.alpha3Code === 'GUF').area = 83534;
-  countryData.find(x => x.alpha3Code === 'MTQ').area = 1128;
-  countryData.find(x => x.alpha3Code === 'GLP').area = 1628;
-  countryData.find(x => x.alpha3Code === 'SJM').area = 62049;
-
-  countryData.find(x => x.alpha3Code === 'KOS').numericCode = 999;
-  countryData.find(x => x.alpha3Code === 'KOS').alpha2Code = 'KO';
+  dataCorrections.forEach((obj) => {
+    countryData.find(x => x.alpha3Code === obj.alpha)[obj.attribute] = obj.value;
+  });
 
   // Create geography paths for regions of France
   const france = geographyPath.find(x => x.id === '250');
@@ -144,139 +150,57 @@ const DataFix = (geoPath, data, capitalMarkers) => {
 const CountryMarkersFix = (centroids) => {
   const centroidsData = centroids;
 
-  centroidsData.find(x => x.alpha3Code === 'CAN').coordinates = [-100, 55];
-  centroidsData.find(x => x.alpha3Code === 'USA').coordinates = [-100, 40];
-  centroidsData.find(x => x.alpha3Code === 'CHL').coordinates = [-73, -39];
-  centroidsData.find(x => x.alpha3Code === 'FJI').coordinates = [177.5, -18];
-  centroidsData.find(x => x.alpha3Code === 'KIR').coordinates = [189, -1];
-  centroidsData.find(x => x.alpha3Code === 'MHL').coordinates = [169, 8.5];
-  centroidsData.find(x => x.alpha3Code === 'FSM').coordinates = [151, 7.5];
-  centroidsData.find(x => x.alpha3Code === 'MNP').coordinates = [145.5, 16.5];
-  centroidsData.find(x => x.alpha3Code === 'SLB').coordinates = [161.6, -9.75];
-  centroidsData.find(x => x.alpha3Code === 'VUT').coordinates = [168.5, -17];
-  centroidsData.find(x => x.alpha3Code === 'NCL').coordinates = [163.8, -20.9];
-  centroidsData.find(x => x.alpha3Code === 'PLW').coordinates = [133, 6];
+  const centroidsFix = [
+    // Americas coordinates
+    ['CAN', [-100, 55]], ['USA', [-100, 40]], ['CHL', [-73, -39]],
+    // Oceania coordinates
+    ['FJI', [177.5, -18]], ['KIR', [189, -1]], ['MHL', [169, 8.5]], ['FSM', [151, 7.5]], ['MNP', [145.5, 16.5]],
+    ['SLB', [161.6, -9.75]], ['VUT', [168.5, -17]], ['NCL', [163.8, -20.9]], ['PLW', [133, 6]],
+    // South America and Africa marker offsets
+    ['SUR', -10], ['GUY', -15], ['DOM', 10], ['GMB', 3], ['GNB', 5], ['GIN', 5], ['SLE', 5], ['LBR', 5],
+    ['NGA', -5], ['CIV', 22], ['GHA', 10], ['TGO', 5], ['CAF', 5], ['CMR', 10], ['COD', -17], ['COG', 10],
+    ['KEN', 10], ['COM', -5], ['MUS', -5], ['ZAF', -8], ['MWI', -5],
+    // Europe marker offsets
+    ['AUT', -5], ['CHE', 5], ['SVN', -3], ['HRV', -5], ['BIH', -7], ['SRB', 3], ['MNE', -5], ['ALB', 5],
+    // Asia marker offsets
+    ['ISR', 10], ['JOR', 10], ['LBN', 5], ['GEO', -5], ['ARM', -8], ['TKM', 5], ['AZE', 3], ['BRN', -5], ['BRN', -5],
+    // Oceania marker offsets
+    ['WLF', -10], ['ASM', 10],
+  ];
 
-  centroidsData.find(x => x.alpha3Code === 'SUR').markerOffset = -10;
-  centroidsData.find(x => x.alpha3Code === 'GUY').markerOffset = -15;
-  centroidsData.find(x => x.alpha3Code === 'DOM').markerOffset = 10;
-  centroidsData.find(x => x.alpha3Code === 'GMB').markerOffset = 3;
-  centroidsData.find(x => x.alpha3Code === 'GNB').markerOffset = 5;
-  centroidsData.find(x => x.alpha3Code === 'GIN').markerOffset = 5;
-  centroidsData.find(x => x.alpha3Code === 'SLE').markerOffset = 5;
-  centroidsData.find(x => x.alpha3Code === 'LBR').markerOffset = 5;
-  centroidsData.find(x => x.alpha3Code === 'NGA').markerOffset = -5;
-  centroidsData.find(x => x.alpha3Code === 'CIV').markerOffset = 22;
-  centroidsData.find(x => x.alpha3Code === 'GHA').markerOffset = 10;
-  centroidsData.find(x => x.alpha3Code === 'TGO').markerOffset = 5;
-  centroidsData.find(x => x.alpha3Code === 'CAF').markerOffset = 5;
-  centroidsData.find(x => x.alpha3Code === 'CMR').markerOffset = 10;
-  centroidsData.find(x => x.alpha3Code === 'COD').markerOffset = -17;
-  centroidsData.find(x => x.alpha3Code === 'COG').markerOffset = 10;
-  centroidsData.find(x => x.alpha3Code === 'KEN').markerOffset = 10;
-  centroidsData.find(x => x.alpha3Code === 'COM').markerOffset = -5;
-  centroidsData.find(x => x.alpha3Code === 'MUS').markerOffset = -5;
-  centroidsData.find(x => x.alpha3Code === 'ZAF').markerOffset = -8;
-  centroidsData.find(x => x.alpha3Code === 'MWI').markerOffset = -5;
-
-  centroidsData.find(x => x.alpha3Code === 'AUT').markerOffset = -5;
-  centroidsData.find(x => x.alpha3Code === 'CHE').markerOffset = 5;
-  centroidsData.find(x => x.alpha3Code === 'SVN').markerOffset = -3;
-  centroidsData.find(x => x.alpha3Code === 'HRV').markerOffset = -5;
-  centroidsData.find(x => x.alpha3Code === 'BIH').markerOffset = -7;
-  centroidsData.find(x => x.alpha3Code === 'SRB').markerOffset = 3;
-  centroidsData.find(x => x.alpha3Code === 'MNE').markerOffset = -5;
-  centroidsData.find(x => x.alpha3Code === 'ALB').markerOffset = 5;
-
-  centroidsData.find(x => x.alpha3Code === 'ISR').markerOffset = 10;
-  centroidsData.find(x => x.alpha3Code === 'JOR').markerOffset = 10;
-  centroidsData.find(x => x.alpha3Code === 'LBN').markerOffset = 5;
-  centroidsData.find(x => x.alpha3Code === 'GEO').markerOffset = -5;
-  centroidsData.find(x => x.alpha3Code === 'ARM').markerOffset = -8;
-  centroidsData.find(x => x.alpha3Code === 'TKM').markerOffset = 5;
-  centroidsData.find(x => x.alpha3Code === 'AZE').markerOffset = 3;
-  centroidsData.find(x => x.alpha3Code === 'BRN').markerOffset = -5;
-  centroidsData.find(x => x.alpha3Code === 'BRN').markerOffset = -5;
-
-  centroidsData.find(x => x.alpha3Code === 'WLF').markerOffset = -10;
-  centroidsData.find(x => x.alpha3Code === 'ASM').markerOffset = 10;
+  centroidsFix.forEach((fix) => {
+    if (Array.isArray(fix[1])) {
+      centroidsData.find(x => x.alpha3Code === fix[0]).coordinates = fix[1];
+    } else {
+      centroidsData.find(x => x.alpha3Code === fix[0]).markerOffset = fix[1];
+    }
+  });
 };
 
 const CapitalMarkersFix = (capitalMarkers) => {
   const capitalMarkersData = capitalMarkers;
 
-  capitalMarkersData.find(x => x.alpha3Code === 'GTM').markerOffset = 10;
-  capitalMarkersData.find(x => x.alpha3Code === 'SLV').markerOffset = 12;
-  capitalMarkersData.find(x => x.alpha3Code === 'CRI').markerOffset = 12;
-  capitalMarkersData.find(x => x.alpha3Code === 'URY').markerOffset = 15;
-  capitalMarkersData.find(x => x.alpha3Code === 'GUY').markerOffset = -10;
-  capitalMarkersData.find(x => x.alpha3Code === 'SUR').markerOffset = -5;
-  capitalMarkersData.find(x => x.alpha3Code === 'GUF').markerOffset = 0;
-  capitalMarkersData.find(x => x.alpha3Code === 'DOM').markerOffset = 0;
+  const capitalFix = [
+    // Caribbean capital markers fix
+    ['GTM', 10], ['SLV', 12], ['CRI', 12], ['URY', 15], ['GUY', -10], ['SUR', -5], ['GUF', 0], ['DOM', 0],
+    // Africa capital markers fix
+    ['CPV', -10], ['SEN', 0], ['GMB', 2], ['GNB', 2], ['GIN', 2], ['SLE', 6], ['LBR', 4], ['BFA', 13],
+    ['CIV', -10], ['GHA', 12], ['TGO', 5], ['BEN', -3], ['NGA', 0], ['STP', -5], ['GNQ', 0], ['GAB', 10],
+    ['COG', -5], ['COD', 12], ['CAF', -10], ['ERI', 0], ['BDI', 12], ['MYT', 13], ['REU', 13], ['ZAF', -3],
+    ['LSO', 13], ['SWZ', 13],
+    // Europe capital markers fix
+    ['EST', 13], ['SVK', 13], ['HUN', 13], ['HRV', 8], ['SVN', 0], ['CHE', 0], ['AND', 0], ['MCO', 0],
+    ['VAT', 15], ['BIH', 0], ['ALB', 12], ['MKD', 12], ['MNE', 0], ['BGR', 5],
+    // Asia capital markers fix
+    ['ARM', 13], ['PSE', 13], ['JOR', 3], ['IRQ', -3], ['KWT', -3], ['SYR', -12], ['OMN', 12], ['ARE', -3],
+    ['AFG', -9], ['PAK', -5], ['IND', -9], ['NPL', -5], ['BTN', 13], ['BGD', 13], ['KOR', 13],
+    // Oceania capital markers fix
+    ['GUM', 13], ['KIR', 13], ['ASM', 13], ['WLF', -10], ['WSM', -3],
+  ];
 
-  capitalMarkersData.find(x => x.alpha3Code === 'CPV').markerOffset = -10;
-  capitalMarkersData.find(x => x.alpha3Code === 'SEN').markerOffset = 0;
-  capitalMarkersData.find(x => x.alpha3Code === 'GMB').markerOffset = 2;
-  capitalMarkersData.find(x => x.alpha3Code === 'GNB').markerOffset = 2;
-  capitalMarkersData.find(x => x.alpha3Code === 'GIN').markerOffset = 2;
-  capitalMarkersData.find(x => x.alpha3Code === 'SLE').markerOffset = 6;
-  capitalMarkersData.find(x => x.alpha3Code === 'LBR').markerOffset = 4;
-  capitalMarkersData.find(x => x.alpha3Code === 'BFA').markerOffset = 13;
-  capitalMarkersData.find(x => x.alpha3Code === 'CIV').markerOffset = -10;
-  capitalMarkersData.find(x => x.alpha3Code === 'GHA').markerOffset = 12;
-  capitalMarkersData.find(x => x.alpha3Code === 'TGO').markerOffset = 5;
-  capitalMarkersData.find(x => x.alpha3Code === 'BEN').markerOffset = -3;
-  capitalMarkersData.find(x => x.alpha3Code === 'NGA').markerOffset = 0;
-  capitalMarkersData.find(x => x.alpha3Code === 'STP').markerOffset = -5;
-  capitalMarkersData.find(x => x.alpha3Code === 'GNQ').markerOffset = 0;
-  capitalMarkersData.find(x => x.alpha3Code === 'GAB').markerOffset = 10;
-  capitalMarkersData.find(x => x.alpha3Code === 'COG').markerOffset = -5;
-  capitalMarkersData.find(x => x.alpha3Code === 'COD').markerOffset = 12;
-  capitalMarkersData.find(x => x.alpha3Code === 'CAF').markerOffset = -10;
-  capitalMarkersData.find(x => x.alpha3Code === 'ERI').markerOffset = 0;
-  capitalMarkersData.find(x => x.alpha3Code === 'BDI').markerOffset = 12;
-  capitalMarkersData.find(x => x.alpha3Code === 'MYT').markerOffset = 13;
-  capitalMarkersData.find(x => x.alpha3Code === 'REU').markerOffset = 13;
-  capitalMarkersData.find(x => x.alpha3Code === 'ZAF').markerOffset = -3;
-  capitalMarkersData.find(x => x.alpha3Code === 'LSO').markerOffset = 13;
-  capitalMarkersData.find(x => x.alpha3Code === 'SWZ').markerOffset = 13;
-
-  capitalMarkersData.find(x => x.alpha3Code === 'EST').markerOffset = 13;
-  capitalMarkersData.find(x => x.alpha3Code === 'SVK').markerOffset = 13;
-  capitalMarkersData.find(x => x.alpha3Code === 'HUN').markerOffset = 13;
-  capitalMarkersData.find(x => x.alpha3Code === 'HRV').markerOffset = 8;
-  capitalMarkersData.find(x => x.alpha3Code === 'SVN').markerOffset = 0;
-  capitalMarkersData.find(x => x.alpha3Code === 'CHE').markerOffset = 0;
-  capitalMarkersData.find(x => x.alpha3Code === 'AND').markerOffset = 0;
-  capitalMarkersData.find(x => x.alpha3Code === 'MCO').markerOffset = 0;
-  capitalMarkersData.find(x => x.alpha3Code === 'VAT').markerOffset = 15;
-  capitalMarkersData.find(x => x.alpha3Code === 'BIH').markerOffset = 0;
-  capitalMarkersData.find(x => x.alpha3Code === 'ALB').markerOffset = 12;
-  capitalMarkersData.find(x => x.alpha3Code === 'MKD').markerOffset = 12;
-  capitalMarkersData.find(x => x.alpha3Code === 'MNE').markerOffset = 0;
-  capitalMarkersData.find(x => x.alpha3Code === 'BGR').markerOffset = 5;
-
-  capitalMarkersData.find(x => x.alpha3Code === 'ARM').markerOffset = 13;
-  capitalMarkersData.find(x => x.alpha3Code === 'PSE').markerOffset = 13;
-  capitalMarkersData.find(x => x.alpha3Code === 'JOR').markerOffset = 3;
-  capitalMarkersData.find(x => x.alpha3Code === 'IRQ').markerOffset = -3;
-  capitalMarkersData.find(x => x.alpha3Code === 'KWT').markerOffset = -3;
-  capitalMarkersData.find(x => x.alpha3Code === 'SYR').markerOffset = -12;
-  capitalMarkersData.find(x => x.alpha3Code === 'OMN').markerOffset = 12;
-  capitalMarkersData.find(x => x.alpha3Code === 'ARE').markerOffset = -3;
-  capitalMarkersData.find(x => x.alpha3Code === 'AFG').markerOffset = -9;
-  capitalMarkersData.find(x => x.alpha3Code === 'PAK').markerOffset = -5;
-  capitalMarkersData.find(x => x.alpha3Code === 'IND').markerOffset = -9;
-  capitalMarkersData.find(x => x.alpha3Code === 'NPL').markerOffset = -5;
-  capitalMarkersData.find(x => x.alpha3Code === 'BTN').markerOffset = 13;
-  capitalMarkersData.find(x => x.alpha3Code === 'BGD').markerOffset = 13;
-  capitalMarkersData.find(x => x.alpha3Code === 'KOR').markerOffset = 13;
-
-  capitalMarkersData.find(x => x.alpha3Code === 'GUM').markerOffset = 13;
-  capitalMarkersData.find(x => x.alpha3Code === 'KIR').markerOffset = 13;
-  capitalMarkersData.find(x => x.alpha3Code === 'ASM').markerOffset = 13;
-  capitalMarkersData.find(x => x.alpha3Code === 'WLF').markerOffset = -10;
-  capitalMarkersData.find(x => x.alpha3Code === 'WSM').markerOffset = -3;
+  capitalFix.forEach((fix) => {
+    capitalMarkersData.find(x => x.alpha3Code === fix[0]).markerOffset = fix[1];
+  });
 };
 
 function SeparateRegions(data) {
