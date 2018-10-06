e7c78fa6532833549313d69b4e2b28d46f4add0f
dev637 <devang713@gmail.com>
2018-10-06 18:01:10 -0400

    Adjust default center points for region selector
    Adjusted default center points for South America and Africa regions, as upper capital labels were blocked by progress bar during quizzes.

--- src/assets/regionAlpha3Codes.js
+++ src/assets/regionAlpha3Codes.js
@@ -12,9 +12,9 @@ const alpha3Codes = {
 const mapConfig = {
   world: { center: [10, 0], zoom: 1 },
   naca: { center: [-95, 30], zoom: 2.5 },
-  'South America': { center: [-65, -30], zoom: 2 },
+  'South America': { center: [-65, -28], zoom: 1.9 },
   Caribbean: { center: [-70, 17], zoom: 8.5 },
-  Africa: { center: [10, -5], zoom: 2 },
+  Africa: { center: [10, -3], zoom: 1.8 },
   Europe: { center: [5, 50], zoom: 3.5 },
   Asia: { center: [90, 20], zoom: 2 },
   Oceania: { center: [-190, -22], zoom: 2 },
