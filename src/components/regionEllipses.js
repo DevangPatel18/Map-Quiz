fd0cf9a748b9972c5f8731182d94011848f5cca7
dev637 <devang713@gmail.com>
2018-10-23 13:11:04 -0400

    Reduce ellipse size on Caribbean nations
    Reduced ellipse size of Caribbean nations on full map quizzes to prevent overlapping shapes.

--- src/components/regionEllipses.js
+++ src/components/regionEllipses.js
@@ -5,6 +5,7 @@ import ColorPicker from './colorPicker';
 import { ellipseDim, labelDist } from '../helpers/markerParams';
 
 const oceaniaUN = ['PLW', 'FSM', 'MHL', 'KIR', 'NRU', 'SLB', 'NCL', 'VUT', 'FJI', 'TON', 'WSM'];
+const caribUN = ['ATG', 'BRB', 'DMA', 'GRD', 'KNA', 'LCA', 'VCT'];
 export default function regionEllipses() {
   const {
     currentMap, geographyPaths, filterRegions, capitalMarkers, countryMarkers, zoom, quiz,
@@ -18,7 +19,7 @@ export default function regionEllipses() {
       minArea = 29000;
       break;
     default:
-      minArea = 6000;
+      minArea = 5000;
   }
 
   let filterFN;
@@ -53,8 +54,12 @@ export default function regionEllipses() {
           const bounds = path.bounds(country);
           const originWidth = bounds[1][0] - bounds[0][0];
           const originHeight = bounds[1][1] - bounds[0][1];
-          widthMain = Math.max(originWidth, 3);
-          heightMain = Math.max(originHeight, 3);
+          
+          const radius = caribUN.includes(alpha3Code) ? 1.5 : 3;
+          
+          widthMain = Math.max(originWidth, radius);
+          heightMain = Math.max(originHeight, radius);
+
           angleMain = 0;
         }
 
