3c36e143fb324a9dc3ccb5957642759fae8f1bf6
dev637 <devang713@gmail.com>
2018-10-24 20:52:33 -0400

    Create styled component for mobile message
    Created styled component for mobile message and increased z-index to prevent overlapping from quiz menu on small mobile devices.

--- src/components/mobileMessage.js
+++ src/components/mobileMessage.js
@@ -1,34 +1,37 @@
 import React, { Component } from 'react';
 import { Message } from 'semantic-ui-react';
+import styled from 'styled-components';
 
-const mobileMessageStyle = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%,-50%)'
-}
+const MessageStyle = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 2;
+`;
 
 class MobileMessage extends Component {
-  state = { visible: true }
+  state = { visible: true };
 
   handleDismiss = () => {
-    this.setState({ visible: false })
-  }
+    this.setState({ visible: false });
+  };
 
   render() {
     if (this.state.visible) {
       return (
-        <Message
-          onDismiss={this.handleDismiss}
-          header='Welcome to Map Quiz!'
-          content="Noticed you're on a mobile device. You can move around the map using two finger panning and zoom in/out via the buttons in the top left corner."
-          style={mobileMessageStyle}
-          size="small"
-        />
-      )
+        <MessageStyle>
+          <Message
+            onDismiss={this.handleDismiss}
+            header="Welcome to Map Quiz!"
+            content="Noticed you're on a mobile device. You can move around the map using two finger panning and zoom in/out via the buttons in the top left corner."
+            size="small"
+          />
+        </MessageStyle>
+      );
     }
 
-    return null
+    return null;
   }
 }
 
