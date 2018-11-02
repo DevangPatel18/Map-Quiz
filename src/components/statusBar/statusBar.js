7df72bb365ba171abe347df5145e8c9289711fc6
dev637 <devang713@gmail.com>
2018-11-02 17:30:26 -0400

    Change StatusBar to stateless functional component

--- src/components/statusBar/statusBar.js
+++ src/components/statusBar/statusBar.js
@@ -1,36 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Progress } from 'semantic-ui-react';
 import { isMobile } from 'react-device-detect';
 import StatusBarStyles from '../styles/StatusBarStyles';
 
-class StatusBar extends Component {
-  render() {
-    const { status } = this.props;
-    const { quiz, quizGuesses, quizAnswers } = status;
-    const percentComp = quiz
-      ? parseInt((quizGuesses.length / quizAnswers.length) * 100, 10)
-      : '';
-    const questionText = `Question: ${quizGuesses.length} / ${
-      quizAnswers.length
-    }`;
-    const scoreText = `Score: ${quizGuesses.filter(x => x).length}`;
+const StatusBar = props => {
+  const { status } = props;
+  const { quiz, quizGuesses, quizAnswers } = status;
+  const percentComp = quiz
+    ? parseInt((quizGuesses.length / quizAnswers.length) * 100, 10)
+    : '';
+  const questionText = `Question: ${quizGuesses.length} / ${
+    quizAnswers.length
+  }`;
+  const scoreText = `Score: ${quizGuesses.filter(x => x).length}`;
 
-    return (
-      <div>
-        <StatusBarStyles quiz={quiz} isMobile={isMobile}>
-          <Progress
-            percent={percentComp}
-            className="statusBar-progress"
-            progress
-          />
-          <div className="statusBar-ratio">
-            <p>{questionText}</p>
-            <p>{scoreText}</p>
-          </div>
-        </StatusBarStyles>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <StatusBarStyles quiz={quiz} isMobile={isMobile}>
+        <Progress
+          percent={percentComp}
+          className="statusBar-progress"
+          progress
+        />
+        <div className="statusBar-ratio">
+          <p>{questionText}</p>
+          <p>{scoreText}</p>
+        </div>
+      </StatusBarStyles>
+    </div>
+  );
+};
 
 export default StatusBar;
