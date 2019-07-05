84f37606fdb2742915a2a9bf954b20bf7e944bb0
dev637 <devang713@gmail.com>
2019-07-05 14:20:27 -0400

    Change statusBar into stateful component

--- src/components/statusBar/statusBar.js
+++ src/components/statusBar/statusBar.js
@@ -1,35 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Progress } from 'semantic-ui-react';
 import { isMobile } from 'react-device-detect';
 import StatusBarStyles from '../styles/StatusBarStyles';
 
-const StatusBar = props => {
-  const { quiz, quizGuesses, quizAnswers } = props.quiz;
-  const percentComp = quiz
-    ? parseInt((quizGuesses.length / quizAnswers.length) * 100, 10)
-    : '';
-  const questionText = `Question: ${quizGuesses.length} / ${
-    quizAnswers.length
-  }`;
-  const scoreText = `Score: ${quizGuesses.filter(x => x).length}`;
+class StatusBar extends Component {
+  constructor() {
+    super();
+    
+    this.state = {};
+  }
+  render() {
+    const { quiz, quizGuesses, quizAnswers } = this.props.quiz;
+    const percentComp = quiz
+      ? parseInt((quizGuesses.length / quizAnswers.length) * 100, 10)
+      : '';
+    const questionText = `Question: ${quizGuesses.length} / ${
+      quizAnswers.length
+    }`;
+    const scoreText = `Score: ${quizGuesses.filter(x => x).length}`;
 
-  return (
-    <div>
-      <StatusBarStyles quiz={quiz} mobile={isMobile}>
-        <Progress
-          percent={percentComp}
-          className="statusBar-progress"
-          progress
-        />
-        <div className="statusBar-ratio">
-          <p>{questionText}</p>
-          <p>{scoreText}</p>
-        </div>
-      </StatusBarStyles>
-    </div>
-  );
-};
+    return (
+      <div>
+        <StatusBarStyles quiz={quiz} mobile={isMobile}>
+          <Progress
+            percent={percentComp}
+            className="statusBar-progress"
+            progress
+          />
+          <div className="statusBar-ratio">
+            <p>{questionText}</p>
+            <p>{scoreText}</p>
+          </div>
+        </StatusBarStyles>
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = state => ({
   quiz: state.quiz,
