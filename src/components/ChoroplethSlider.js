83133d4308afbcf2919b38a6ab3730d7a336d617
dev637 <devang713@gmail.com>
2019-08-06 14:15:43 -0400

    Attach setChoroYear dispatcher to range slider

--- src/components/ChoroplethSlider.js
+++ src/components/ChoroplethSlider.js
@@ -1,19 +1,31 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import ChoroplethSliderStyles from './styles/ChoroplethSliderStyles';
+import { setChoroYear } from '../actions/mapActions';
 
-const ChoroplethSlider = () => {
+const ChoroplethSlider = props => {
+  const { setChoroYear } = props;
+  const { sliderYear } = props.map;
+  console.log(sliderYear);
   return (
     <ChoroplethSliderStyles>
       <input
         type="range"
         min="1960"
         max="2018"
-        // value="2018"
+        value={sliderYear}
         name="choroplethSlider"
-        onChange={e => console.log(e.target.value)}
+        onChange={e => setChoroYear(e.target.value)}
       />
     </ChoroplethSliderStyles>
   );
 };
 
-export default ChoroplethSlider;
+const mapStateToProps = state => ({
+  map: state.map,
+});
+
+export default connect(
+  mapStateToProps,
+  { setChoroYear }
+)(ChoroplethSlider);
