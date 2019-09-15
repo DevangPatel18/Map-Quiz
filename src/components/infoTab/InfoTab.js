86cfe3461cd3addcf7b41af59e560bc671dd1538
dev637 <devang713@gmail.com>
2019-09-15 12:52:57 -0400

    Change InfoTab to class component
    Change InfoTab to class component to allow preloading of flag image before component render.

--- src/components/infoTab/InfoTab.js
+++ src/components/infoTab/InfoTab.js
@@ -1,37 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import InfoTabStyles from '../styles/InfoTabStyles';
 
-const InfoTab = ({ regionData, quiz, data }) => {
-  const { infoTabShow } = quiz;
-  const { geographyPaths } = data;
-  let { name, capital, population, area, regionOf } = regionData;
-  let regionOfStr = '';
-  population = population ? `${population.toLocaleString()}` : 'N/A';
-  area = area ? `${area.toLocaleString()} km²` : 'N/A';
-  if (regionOf) {
-    const regionName = geographyPaths.find(
-      x => x.properties.alpha3Code === regionOf
-    ).properties.name;
-    regionOfStr = `${regionName}`;
+class InfoTab extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isFlagImgReady: false };
   }
-  return (
-    <InfoTabStyles infoTabShow={infoTabShow}>
-      <img
-        className="infoTab-flag"
-        src={regionData.flag}
-        alt={`${name}-flag`}
-      />
-      <div className="infoTab-desc">
-        <li>{name}</li>
-        <li>Capital: {capital}</li>
-        <li>Population: {population}</li>
-        <li>Area: {area}</li>
-        {regionOfStr !== '' ? <li>Region of {regionOfStr}</li> : ''}
-      </div>
-    </InfoTabStyles>
-  );
-};
+
+  componentDidMount() {
+    const flagImg = new Image();
+    flagImg.src = this.props.regionData.flag;
+
+    flagImg.onload = () => {
+      this.setState({ isFlagImgReady: true });
+    };
+  }
+
+  render() {
+    if (!this.state.isFlagImgReady) return '';
+    const { regionData, data } = this.props;
+    const { geographyPaths } = data;
+    let { name, capital, population, area, regionOf } = regionData;
+    let regionOfStr = '';
+    population = population ? `${population.toLocaleString()}` : 'N/A';
+    area = area ? `${area.toLocaleString()} km²` : 'N/A';
+    if (regionOf) {
+      const regionName = geographyPaths.find(
+        x => x.properties.alpha3Code === regionOf
+      ).properties.name;
+      regionOfStr = `${regionName}`;
+    }
+    return (
+      <InfoTabStyles>
+        <img
+          className="infoTab-flag"
+          src={regionData.flag}
+          alt={`${name}-flag`}
+        />
+        <div className="infoTab-desc">
+          <li>{name}</li>
+          <li>Capital: {capital}</li>
+          <li>Population: {population}</li>
+          <li>Area: {area}</li>
+          {regionOfStr !== '' ? <li>Region of {regionOfStr}</li> : ''}
+        </div>
+      </InfoTabStyles>
+    );
+  }
+}
 
 const mapStateToProps = state => ({
   data: state.data,
