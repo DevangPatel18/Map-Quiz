e932911fdbb11ebf29a0f9cd4ec8812ba07afd9e
dev637 <devang713@gmail.com>
2019-09-18 12:42:46 -0400

    Add getRegionEllipses to dataActions

--- src/actions/dataActions.js
+++ src/actions/dataActions.js
@@ -1,4 +1,4 @@
-import { LOAD_PATHS, LOAD_DATA, DISABLE_OPT } from './types';
+import { LOAD_PATHS, LOAD_DATA, DISABLE_OPT, GET_ELLIPSES } from './types';
 import { modifyWorldGeographyPaths } from '../helpers/attributeFix';
 import {
   getWorldTopology,
@@ -6,6 +6,12 @@ import {
   getPopulationData,
   getWorldDataSet,
 } from '../helpers/dataActionHelpers';
+import {
+  getFilterFunction,
+  getEllipseMarkerProperties,
+  getCaribbeanMarkerProperties,
+} from '../helpers/regionEllipsesHelpers';
+import store from '../store';
 
 export const loadPaths = () => async dispatch => {
   const worldTopology = await getWorldTopology();
@@ -30,4 +36,31 @@ export const loadData = () => async dispatch => {
   });
 
   dispatch({ type: DISABLE_OPT });
+};
+
+export const getRegionEllipses = currentMap => dispatch => {
+  const { map, data } = store.getState();
+  const { filterRegions } = map;
+  const { geographyPaths, regionEllipsesData } = data;
+  const filterFunc = getFilterFunction(currentMap);
+  const markersArray = geographyPaths
+    .filter(x => filterRegions.includes(x.properties.alpha3Code))
+    .filter(filterFunc)
+    .map(region => {
+      const { alpha3Code } = region.properties;
+      const caribbeanMap = currentMap === 'Caribbean';
+      const markerData = caribbeanMap
+        ? getCaribbeanMarkerProperties(alpha3Code)
+        : getEllipseMarkerProperties(region);
+      markerData.region = region;
+      return markerData;
+    });
+
+  dispatch({
+    type: GET_ELLIPSES,
+    regionEllipsesData: {
+      ...regionEllipsesData,
+      [currentMap]: markersArray,
+    },
+  });
 };
