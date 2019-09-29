ca5b8df27bb92597c4f2bccadc7aa805b2e54f2a
dev637 <devang713@gmail.com>
2019-09-29 13:17:05 -0400

    Refactor RegionSearch to use regionSearchList

--- src/components/RegionSearch.js
+++ src/components/RegionSearch.js
@@ -4,68 +4,26 @@ import { connect } from 'react-redux';
 import { createSelector } from 'reselect';
 import { regionZoom } from '../actions/mapActions';
 
-const RegionSearch = ({ map, data, regionZoom }) => {
-  const { currentMap, filterRegions, regionKey } = map;
-  const { geographyPaths } = data;
-
-  let mapRegions = geographyPaths;
-  if (currentMap !== 'World') {
-    mapRegions = mapRegions.filter(x =>
-      filterRegions.includes(x.properties[regionKey])
-    );
-  }
-
-  mapRegions = mapRegions
-    .map(x => {
-      let key;
-      let flag;
-      if (regionKey === 'alpha3Code') {
-        if (!x.properties.alpha2Code) {
-          return null;
-        }
-        key = x.properties.alpha2Code.toString().toLowerCase();
-        flag = { flag: key };
-      } else {
-        key = x.properties[regionKey];
-      }
-
-      return {
-        key,
-        ...flag,
-        text: x.properties.name,
-        value: x.properties[regionKey],
-      };
-    })
-    .filter(x => x !== null)
-    .filter(
-      x =>
-        !['bl', 'cw', 'gg', 'im', 'je', 'mf', 'ss', 'sx', 'bq', 'ko'].includes(x.key)
-    )
-    .sort((a, b) => (a.text > b.text ? 1 : -1));
-
-  return (
-    <div className="regionSearch">
-      <Dropdown
-        aria-label="user country search"
-        placeholder="Select Country"
-        fluid
-        search
-        selection
-        options={mapRegions}
-        onChange={regionZoom}
-      />
-    </div>
-  );
-};
+const RegionSearch = ({ currentMap, regionSearchList, regionZoom }) => (
+  <div className="regionSearch">
+    <Dropdown
+      aria-label="user country search"
+      placeholder="Select Country"
+      fluid
+      search
+      selection
+      options={regionSearchList[currentMap] || []}
+      onChange={regionZoom}
+    />
+  </div>
+);
 
 const getAppState = createSelector(
   state => state.map.currentMap,
-  state => state.map.filterRegions,
-  state => state.map.regionKey,
-  state => state.data.geographyPaths,
-  (currentMap, filterRegions, regionKey, geographyPaths) => ({
-    map: { currentMap, filterRegions, regionKey },
-    data: { geographyPaths },
+  state => state.data.regionSearchList,
+  (currentMap, regionSearchList) => ({
+    currentMap,
+    regionSearchList,
   })
 );
 
