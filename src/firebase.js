f4e503040438639560a7bbfac55131c73b9051ca
dev637 <devang713@gmail.com>
2019-11-28 16:31:31 -0500

    Connect firebase for sub division data retrieval

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase';
+
+const config = {
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASEURL,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
+  measurementId: process.env.REACT_APP_MEASUREMENTID,
+};
+
+firebase.initializeApp(config);
+
+export const getFirebaseRegionData = async regionName =>
+  await firebase
+    .database()
+    .ref(`/${regionName}`)
+    .once('value')
+    .then(data => data.val());
