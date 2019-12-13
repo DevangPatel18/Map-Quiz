92c66d68c2d8e1ed16f1333fc668c96a94e70c04
dev637 <devang713@gmail.com>
2019-12-13 17:00:47 -0500

    Update introduction tab in RegionModal

--- src/helpers/RegionModalHelpers.js
+++ src/helpers/RegionModalHelpers.js
@@ -3,11 +3,18 @@ import { Tab } from 'semantic-ui-react';
 import JSONTree from 'react-json-tree';
 import { theme } from '../components/styles/RegionModalStyles';
 
-export const generateTabContent = (displayData, sectionName) => (
-  <Tab.Pane>
-    <JSONTree data={displayData[sectionName]} theme={theme} />
-  </Tab.Pane>
-);
+export const generateTabContent = (displayData, sectionName) => {
+  switch (sectionName) {
+    case 'introduction':
+      return generateParagraphs(displayData[sectionName].background);
+    default:
+      return (
+        <Tab.Pane>
+          <JSONTree data={displayData[sectionName]} theme={theme} />
+        </Tab.Pane>
+      );
+  }
+};
 
 const generateParagraphs = text => {
   const lines = text.split('. ');
