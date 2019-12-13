8926aac9d52b9afc64fa6cfab7f7b93a3843f766
dev637 <devang713@gmail.com>
2019-12-13 16:58:21 -0500

    Add generateParagraphs to RegionModalHelpers

--- src/helpers/RegionModalHelpers.js
+++ src/helpers/RegionModalHelpers.js
@@ -8,3 +8,33 @@ export const generateTabContent = (displayData, sectionName) => (
     <JSONTree data={displayData[sectionName]} theme={theme} />
   </Tab.Pane>
 );
+
+const generateParagraphs = text => {
+  const lines = text.split('. ');
+  let temp = '';
+  const paragraphs = [];
+
+  for (let line of lines) {
+    if (temp.length + line.length > 300) {
+      paragraphs.push(temp);
+      temp = line + '. ';
+    } else {
+      temp += line + '. ';
+    }
+  }
+  paragraphs.push(temp.substring(0, temp.length - 2));
+
+  return (
+    <div
+      style={{
+        margin: '0 auto',
+        maxWidth: '600px',
+        fontSize: '1.2em',
+      }}
+    >
+      {paragraphs.map((line, i) => (
+        <p key={i}>{line}</p>
+      ))}
+    </div>
+  );
+};
