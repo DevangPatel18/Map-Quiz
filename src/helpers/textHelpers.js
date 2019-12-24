c003299c4e45aef73d7dae1e196e3085c5c02f49
dev637 <devang713@gmail.com>
2019-12-24 18:47:14 -0500

    Add generateList and generateTable to textHelpers

--- src/helpers/textHelpers.js
+++ src/helpers/textHelpers.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { List, Table } from 'semantic-ui-react';
 
 export const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1);
 
@@ -28,6 +29,49 @@ export const generateParagraphs = text => {
       {paragraphs.map((line, i) => (
         <p key={i}>{line}</p>
       ))}
+    </div>
+  );
+};
+
+export const generateList = list => (
+  <List bulleted>
+    {list.length > 0 ? (
+      <>
+        {list.map((item, i) => (
+          <List.Item key={i} size="large">
+            {item}
+          </List.Item>
+        ))}
+      </>
+    ) : (
+      <List.Item>none</List.Item>
+    )}
+  </List>
+);
+
+export const generateTable = table => {
+  const items = Object.keys(table[0]);
+
+  return (
+    <div style={{ overflow: 'auto' }}>
+      <Table
+        definition
+        compact
+        unstackable
+        collapsing
+        style={{ margin: '0 auto' }}
+      >
+        <Table.Body>
+          {items.map((item, i) => (
+            <Table.Row key={i}>
+              <Table.Cell>{capitalize(item).replace(/_/g, ' ')}</Table.Cell>
+              {table.map((entry, j) => (
+                <Table.Cell key={j}>{entry[item]}</Table.Cell>
+              ))}
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
     </div>
   );
 };
