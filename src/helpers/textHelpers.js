7d17a0255f4de47e0d43cb0707fbdbd90a5772f7
dev637 <devang713@gmail.com>
2020-01-01 22:16:28 -0500

    Add numScale to textHelpers

--- src/helpers/textHelpers.js
+++ src/helpers/textHelpers.js
@@ -3,6 +3,25 @@ import { List, Table, Header } from 'semantic-ui-react';
 
 export const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1);
 
+const numDescription = ['', ' thousand', ' million', ' billion', ' trillion'];
+
+export const numScale = number => {
+  const tier = (Math.log10(number) / 3) | 0;
+
+  // if zero, we don't need a suffix
+  if (tier === 0) return number.toPrecision(3);
+
+  // get suffix and determine scale
+  const suffix = numDescription[tier];
+  const scale = Math.pow(10, tier * 3);
+
+  // scale the number
+  const scaled = number / scale;
+
+  // format number and add suffix
+  return scaled.toPrecision(3) + suffix;
+};
+
 export const generateParagraphs = text => {
   const lines = text.split('. ');
   let temp = '';
