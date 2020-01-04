b5b5d124fefe1172fa0ad660f1861e1190cf3ad2
dev637 <devang713@gmail.com>
2020-01-04 12:58:35 -0500

    Update Economy tab list

--- src/components/RegionModal/Economy.js
+++ src/components/RegionModal/Economy.js
@@ -3,7 +3,11 @@ import { Container, List } from 'semantic-ui-react';
 import JSONTree from 'react-json-tree';
 import { theme } from '../styles/RegionModalStyles';
 import { generateParagraphs } from '../../helpers/textHelpers';
-import { formatDUVobj, generateImportExportTable } from './EconomyHelpers';
+import {
+  formatAnnualValue,
+  formatDUVobj,
+  generateImportExportTable,
+} from './EconomyHelpers';
 
 const Economy = ({ data }) => {
   const {
@@ -13,6 +17,11 @@ const Economy = ({ data }) => {
     budget,
     imports,
     exports,
+    stock_of_direct_foreign_investment,
+    fiscal_year,
+    population_below_poverty_line,
+    industrial_production_growth_rate,
+    taxes_and_other_revenues,
     ...rest
   } = data;
 
@@ -60,6 +69,61 @@ const Economy = ({ data }) => {
         {dateUnitValueObj.map((obj, i) => (
           <React.Fragment key={i}>{formatDUVobj(obj)}</React.Fragment>
         ))}
+        {stock_of_direct_foreign_investment && (
+          <List.Item as="li">
+            <strong>Stock of direct Foreign investment</strong>
+            <List as="ul">
+              {Object.keys(stock_of_direct_foreign_investment).map(subAttr => (
+                <React.Fragment key={subAttr}>
+                  {formatDUVobj({
+                    attribute: subAttr,
+                    ...stock_of_direct_foreign_investment[subAttr],
+                  })}
+                </React.Fragment>
+              ))}
+            </List>
+          </List.Item>
+        )}
+        {fiscal_year && (
+          <List.Item as="li">
+            <strong>Fiscal Year: </strong>
+            {fiscal_year.start} - {fiscal_year.end}
+          </List.Item>
+        )}
+        {population_below_poverty_line && (
+          <List.Item as="li">
+            <strong>Population below poverty line: </strong>
+            {formatAnnualValue(population_below_poverty_line)}
+            {population_below_poverty_line.note && (
+              <List as="ul">
+                <List.Item as="li">
+                  <em>
+                    {'Note: '}
+                    {population_below_poverty_line.note}
+                  </em>
+                </List.Item>
+              </List>
+            )}
+          </List.Item>
+        )}
+        {industrial_production_growth_rate && (
+          <List.Item as="li">
+            <strong>Industrial production growth rate: </strong>
+            {industrial_production_growth_rate.annual_percentage_increase}% (
+            {industrial_production_growth_rate.date}){' '}
+            <sup>
+              (global rank - {industrial_production_growth_rate.global_rank})
+            </sup>
+          </List.Item>
+        )}
+        {taxes_and_other_revenues && (
+          <List.Item as="li">
+            <strong>Taxes and other revenues: </strong>
+            {taxes_and_other_revenues.percent_of_gdp}% of GDP (
+            {taxes_and_other_revenues.date}){' '}
+            <sup>(global rank - {taxes_and_other_revenues.global_rank})</sup>
+          </List.Item>
+        )}
       </List>
       {generateImportExportTable({ importData, exportData })}
       {isRestTreeNonEmpty && <JSONTree data={abstractObj} theme={theme} />}
