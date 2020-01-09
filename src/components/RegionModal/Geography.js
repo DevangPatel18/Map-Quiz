c44a2e2be896dee9a8c25835dfc39a80c125fcff
dev637 <devang713@gmail.com>
2020-01-09 14:46:51 -0500

    Add Area section to Geography tab

--- src/components/RegionModal/Geography.js
+++ src/components/RegionModal/Geography.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { Container } from 'semantic-ui-react';
 import JSONTree from 'react-json-tree';
 import { theme } from '../styles/RegionModalStyles';
+import { generateArea } from './GeographyHelpers';
 
 const Economy = ({ data }) => {
-  const { ...rest } = data;
+  const { area, ...rest } = data;
 
   const isRestTreeNonEmpty = Object.keys(rest).length !== 0;
 
   return (
     <Container text>
+      {generateArea(area)}
       {isRestTreeNonEmpty && <JSONTree data={rest} theme={theme} />}
     </Container>
   );
