0a4c6a24252de53924f59e655feb57bfb8605925
dev637 <devang713@gmail.com>
2020-01-10 13:49:47 -0500

    Add Elevation section to Geography tab

--- src/components/RegionModal/Geography.js
+++ src/components/RegionModal/Geography.js
@@ -2,16 +2,17 @@ import React from 'react';
 import { Container } from 'semantic-ui-react';
 import JSONTree from 'react-json-tree';
 import { theme } from '../styles/RegionModalStyles';
-import { generateArea } from './GeographyHelpers';
+import { generateArea, generateElevation } from './GeographyHelpers';
 
 const Economy = ({ data }) => {
-  const { area, ...rest } = data;
+  const { area, elevation, ...rest } = data;
 
   const isRestTreeNonEmpty = Object.keys(rest).length !== 0;
 
   return (
     <Container text>
       {generateArea(area)}
+      {generateElevation(elevation)}
       {isRestTreeNonEmpty && <JSONTree data={rest} theme={theme} />}
     </Container>
   );
--- src/components/RegionModal/GeographyHelpers.js
+++ src/components/RegionModal/GeographyHelpers.js
@@ -71,3 +71,44 @@ export const generateArea = data => {
     </>
   );
 };
+
+export const generateElevation = data => {
+  const { mean_elevation, lowest_point, highest_point, note } = data;
+
+  return (
+    <>
+      <Header textAlign="center">Elevation</Header>
+
+      <List bulleted>
+        {lowest_point && (
+          <List.Item>
+            <strong>Lowest point: </strong>
+            {`${lowest_point.name} `}
+            {lowest_point.elevation &&
+              `${lowest_point.elevation.value} ${lowest_point.elevation.units}`}
+          </List.Item>
+        )}
+        {highest_point && (
+          <List.Item>
+            <strong>Highest point: </strong>
+            {`${highest_point.name} `}
+            {highest_point.elevation &&
+              `${highest_point.elevation.value} ${highest_point.elevation.units}`}
+          </List.Item>
+        )}
+        {mean_elevation && (
+          <List.Item>
+            <strong>Mean Elevation: </strong>
+            {`${mean_elevation.value} ${mean_elevation.units}`}
+          </List.Item>
+        )}
+        {note && (
+          <List.Item>
+            <strong>Note: </strong>
+            {note}
+          </List.Item>
+        )}
+      </List>
+    </>
+  );
+};
