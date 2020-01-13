32f80671af3187f26d37fe8988c507d0074e0043
dev637 <devang713@gmail.com>
2020-01-13 12:42:43 -0500

    Add Land section to Geography tab

--- src/components/RegionModal/Geography.js
+++ src/components/RegionModal/Geography.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import { Container } from 'semantic-ui-react';
+import { Container, Header } from 'semantic-ui-react';
 import JSONTree from 'react-json-tree';
 import { theme } from '../styles/RegionModalStyles';
 import {
   generateArea,
   generateElevation,
   generateEnvironment,
+  generateLandUse,
+  generateBoundaries,
 } from './GeographyHelpers';
 
 const Economy = ({ data }) => {
-  const { area, elevation, environment, ...rest } = data;
+  const {
+    area,
+    elevation,
+    environment,
+    land_use,
+    land_boundaries,
+    ...rest
+  } = data;
 
   const isRestTreeNonEmpty = Object.keys(rest).length !== 0;
 
@@ -18,6 +27,18 @@ const Economy = ({ data }) => {
       {generateArea(area)}
       {generateElevation(elevation)}
       {generateEnvironment(environment)}
+      <Header textAlign="center">Land</Header>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'baseline',
+          justifyContent: 'space-evenly',
+          flexWrap: 'wrap',
+        }}
+      >
+        {generateLandUse(land_use)}
+        {generateBoundaries(land_boundaries)}
+      </div>
       {isRestTreeNonEmpty && <JSONTree data={rest} theme={theme} />}
     </Container>
   );
