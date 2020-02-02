50d739e71791e46d6ab66ba54e21cca28aae44bb
dev637 <devang713@gmail.com>
2020-02-02 17:08:00 -0500

    Add health section to People tab

--- src/components/RegionModal/People.js
+++ src/components/RegionModal/People.js
@@ -1,14 +1,38 @@
 import React from 'react';
-import { Accordion } from 'semantic-ui-react';
+import { Accordion, Container, List } from 'semantic-ui-react';
 import JSONTree from 'react-json-tree';
 import { theme } from '../styles/RegionModalStyles';
+import { generatePeopleItem } from './PeopleHelpers';
 
 const People = ({ data }) => {
-  const { ...rest } = data;
+  const {
+    adult_obesity,
+    physicians_density,
+    hospital_bed_density,
+    underweight_children,
+    ...rest
+  } = data;
 
   const isOtherTreeNonEmpty = Object.keys(rest).length !== 0;
 
   const panels = [];
+
+  panels.push({
+    key: 'health',
+    title: 'Health',
+    content: {
+      content: (
+        <Container text>
+          <List bulleted>
+            {generatePeopleItem({ adult_obesity })}
+            {generatePeopleItem({ physicians_density })}
+            {generatePeopleItem({ hospital_bed_density })}
+            {generatePeopleItem({ underweight_children })}
+          </List>
+        </Container>
+      ),
+    },
+  });
 
   if (isOtherTreeNonEmpty) {
     panels.push({
