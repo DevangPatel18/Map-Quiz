36f6a74304cfadd50b66696b6d6c396c576c294c
dev637 <devang713@gmail.com>
2020-02-05 18:23:42 -0500

    Add generateHealthTable to PeopleHelpers

--- src/components/RegionModal/PeopleHelpers.js
+++ src/components/RegionModal/PeopleHelpers.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { List } from 'semantic-ui-react';
+import { List, Header, Table } from 'semantic-ui-react';
 import { capWithSpacing } from '../../helpers/textHelpers';
 
 export const generatePeopleItem = (obj = {}) => {
@@ -18,5 +18,46 @@ export const generatePeopleItem = (obj = {}) => {
       {date && ` (${date})`}
       {global_rank && <sup> (global rank - {global_rank})</sup>}
     </List.Item>
+  );
+};
+
+export const generateHealthTable = (obj = {}) => {
+  const entries = Object.entries(obj);
+  if (entries.length === 0) return '';
+  const [title, dataObj] = entries[0];
+  const { date, improved, unimproved } = dataObj;
+  if (!improved || !unimproved) return '';
+
+  return (
+    <>
+      <Header size="small" textAlign="center">
+        {capWithSpacing(title)}
+        {date && ` (${date})`}
+      </Header>
+      <Table unstackable celled compact definition>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell />
+            <Table.HeaderCell>Rural</Table.HeaderCell>
+            <Table.HeaderCell>Urban</Table.HeaderCell>
+            <Table.HeaderCell>Total</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell>improved</Table.Cell>
+            <Table.Cell>{`${improved?.rural?.value} ${improved?.rural?.units}`}</Table.Cell>
+            <Table.Cell>{`${improved?.urban?.value} ${improved?.urban?.units}`}</Table.Cell>
+            <Table.Cell>{`${improved?.total?.value} ${improved?.total?.units}`}</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>unimproved</Table.Cell>
+            <Table.Cell>{`${unimproved?.rural?.value} ${unimproved?.rural?.units}`}</Table.Cell>
+            <Table.Cell>{`${unimproved?.urban?.value} ${unimproved?.urban?.units}`}</Table.Cell>
+            <Table.Cell>{`${unimproved?.total?.value} ${unimproved?.total?.units}`}</Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      </Table>
+    </>
   );
 };
