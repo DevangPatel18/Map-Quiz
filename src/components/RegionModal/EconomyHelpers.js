69e312099bef382de2eb6d1737d6680c14bd5830
dev637 <devang713@gmail.com>
2020-02-16 12:38:57 -0500

    Refactor generateImportExportTable

--- src/components/RegionModal/EconomyHelpers.js
+++ src/components/RegionModal/EconomyHelpers.js
@@ -104,8 +104,15 @@ export const generateDUVTable = obj => (
   </Table>
 );
 
-export const generateImportExportTable = ({ importData, exportData }) => {
-  if (!importData && !exportData) return '';
+export const generateImportExportTable = (dataObj = {}) => {
+  if (
+    Object.entries(dataObj).length === 0 ||
+    (!dataObj.importData && !dataObj.exportData)
+  )
+    return '';
+
+  const cols = ['importData', 'exportData'];
+
   return (
     <Table
       {...tableProps}
@@ -125,102 +132,67 @@ export const generateImportExportTable = ({ importData, exportData }) => {
           <Table.Cell verticalAlign="middle">
             <RowHeaderTextStyled>Commodities</RowHeaderTextStyled>
           </Table.Cell>
-          <Table.Cell>
-            {importData && importData.commodities && (
-              <>
-                <List bulleted>
-                  {importData.commodities.by_commodity.map(commodity => (
-                    <List.Item key={commodity}>{commodity}</List.Item>
-                  ))}
-                </List>
-                {importData.commodities.date && (
-                  <p>Date: {importData.commodities.date}</p>
-                )}
-              </>
-            )}
-          </Table.Cell>
-          <Table.Cell>
-            {exportData && exportData.commodities && (
-              <>
-                <List bulleted>
-                  {exportData.commodities.by_commodity.map(commodity => (
-                    <List.Item key={commodity}>{commodity}</List.Item>
-                  ))}
-                </List>
-                {exportData.commodities.date && (
-                  <p>Date: {exportData.commodities.date}</p>
-                )}
-              </>
-            )}
-          </Table.Cell>
+          {cols.map(col => (
+            <Table.Cell key={col}>
+              {dataObj[col]?.commodities && (
+                <>
+                  <List bulleted>
+                    {dataObj[col]?.commodities?.by_commodity?.map(commodity => (
+                      <List.Item key={commodity}>{commodity}</List.Item>
+                    ))}
+                  </List>
+                  {dataObj[col]?.commodities?.date && (
+                    <p>Date: {dataObj[col].commodities.date}</p>
+                  )}
+                </>
+              )}
+            </Table.Cell>
+          ))}
         </Table.Row>
         <Table.Row verticalAlign="top">
           <Table.Cell verticalAlign="middle">
             <RowHeaderTextStyled>Partners</RowHeaderTextStyled>
           </Table.Cell>
-          <Table.Cell>
-            {importData && importData.partners && (
-              <>
-                <List bulleted>
-                  {importData.partners.by_country.map(({ name, percent }) => (
-                    <List.Item key={name}>
-                      {name} - {percent}%
-                    </List.Item>
-                  ))}
-                </List>
-                {importData.partners.date && (
-                  <p>Date: {importData.partners.date}</p>
-                )}
-              </>
-            )}
-          </Table.Cell>
-          <Table.Cell>
-            {exportData && exportData.partners && (
-              <>
-                <List bulleted>
-                  {exportData.partners.by_country.map(({ name, percent }) => (
-                    <List.Item key={name}>
-                      {name} - {percent}%
-                    </List.Item>
-                  ))}
-                </List>
-                {exportData.partners.date && (
-                  <p>Date: {exportData.partners.date}</p>
-                )}
-              </>
-            )}
-          </Table.Cell>
+          {cols.map(col => (
+            <Table.Cell key={col}>
+              {dataObj[col]?.partners && (
+                <>
+                  <List bulleted>
+                    {dataObj[col]?.partners?.by_country?.map(
+                      ({ name, percent }) => (
+                        <List.Item key={name}>
+                          {name} - {percent}%
+                        </List.Item>
+                      )
+                    )}
+                  </List>
+                  {dataObj[col]?.partners?.date && (
+                    <p>Date: {dataObj[col]?.partners?.date}</p>
+                  )}
+                </>
+              )}
+            </Table.Cell>
+          ))}
         </Table.Row>
         <Table.Row verticalAlign="top">
           <Table.Cell verticalAlign="middle">
             <RowHeaderTextStyled>Total Value</RowHeaderTextStyled>
           </Table.Cell>
-          <Table.Cell>
-            {importData && importData.total_value && (
-              <List bulleted>
-                {importData.total_value.annual_values.map(
-                  (annual_value, idx) => (
-                    <List.Item key={idx}>
-                      {formatAnnualValue(annual_value)}
-                    </List.Item>
-                  )
-                )}
-              </List>
-            )}
-          </Table.Cell>
-          <Table.Cell>
-            {exportData && exportData.total_value && (
-              <List bulleted>
-                {exportData.total_value.annual_values.map(
-                  (annual_value, idx) => (
-                    <List.Item key={idx}>
-                      {formatAnnualValue(annual_value)}
-                    </List.Item>
-                  )
-                )}
-              </List>
-            )}
-          </Table.Cell>
+          {cols.map(col => (
+            <Table.Cell key={col}>
+              {dataObj[col]?.total_value && (
+                <List bulleted>
+                  {dataObj[col]?.total_value?.annual_values?.map(
+                    (annual_value, idx) => (
+                      <List.Item key={idx}>
+                        {formatAnnualValue(annual_value)}
+                      </List.Item>
+                    )
+                  )}
+                </List>
+              )}
+            </Table.Cell>
+          ))}
         </Table.Row>
       </Table.Body>
     </Table>
