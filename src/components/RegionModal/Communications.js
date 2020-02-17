fdaa2d3bcca695d9f42f5115adb47a0b100f55c8
dev637 <devang713@gmail.com>
2020-02-17 12:43:50 -0500

    Add content to Communications tab

--- src/components/RegionModal/Communications.js
+++ src/components/RegionModal/Communications.js
@@ -1,14 +1,30 @@
 import React from 'react';
-import { Container } from 'semantic-ui-react';
+import { Container, List } from 'semantic-ui-react';
 import JSONTree from 'react-json-tree';
 import { theme } from '../styles/RegionModalStyles';
+import { generateSubListItem } from '../../helpers/textHelpers';
+import { generateValueUnitTable } from './PeopleHelpers';
 
 const Communications = ({ data }) => {
-  const { ...rest } = data;
+  const { broadcast_media, note, telephones, ...rest } = data;
   const isRestTreeNonEmpty = Object.keys(rest).length !== 0;
+
+  let tableData;
+  if (telephones) {
+    const { system, ...rest_telephones } = telephones;
+    tableData = rest_telephones;
+    rest.telephones_system = system;
+  }
 
   return (
     <Container text>
+      <List bulleted>
+        {generateSubListItem({ broadcast_media })}
+        {generateSubListItem({ note })}
+      </List>
+
+      {generateValueUnitTable({ telephones: tableData })}
+
       {isRestTreeNonEmpty && <JSONTree data={rest} theme={theme} />}
     </Container>
   );
