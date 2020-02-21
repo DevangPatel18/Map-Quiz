b9cafbf6ac0b76de5aed3e60d06bc3eb947a1fb7
dev637 <devang713@gmail.com>
2020-02-21 12:24:03 -0500

    Add airports table to Transportation tab

--- src/components/RegionModal/Transportation.js
+++ src/components/RegionModal/Transportation.js
@@ -2,13 +2,27 @@ import React from 'react';
 import { Container } from 'semantic-ui-react';
 import JSONTree from 'react-json-tree';
 import { theme } from '../styles/RegionModalStyles';
+import { generateAirportsTable } from './TransportationHelpers';
 
 const Transportation = ({ data }) => {
-  const { ...rest } = data;
+  const { air_transport, ...rest } = data;
+
+  let airportsTable;
+  if (air_transport) {
+    const { airports, ...air_transport_rest } = air_transport;
+    if (airports) {
+      const { paved, unpaved } = airports;
+      const paved_vs_unpaved = { paved, unpaved };
+      airportsTable = generateAirportsTable(paved_vs_unpaved);
+    }
+    rest.air_transport = air_transport_rest;
+  }
+
   const isRestTreeNonEmpty = Object.keys(rest).length !== 0;
 
   return (
     <Container text>
+      {airportsTable}
       {isRestTreeNonEmpty && <JSONTree data={rest} theme={theme} />}
     </Container>
   );
