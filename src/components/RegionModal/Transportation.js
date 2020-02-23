3aecae29931da0877483cd91931277195feaee8f
dev637 <devang713@gmail.com>
2020-02-23 10:46:40 -0500

    Add national system list to Transportation tab

--- src/components/RegionModal/Transportation.js
+++ src/components/RegionModal/Transportation.js
@@ -2,23 +2,34 @@ import React from 'react';
 import { Container } from 'semantic-ui-react';
 import JSONTree from 'react-json-tree';
 import { theme } from '../styles/RegionModalStyles';
-import { generateAirportsTable } from './TransportationHelpers';
+import {
+  generateAirportsTable,
+  generateNationalSystemList,
+} from './TransportationHelpers';
 
 const Transportation = ({ data }) => {
   const { air_transport, ...rest } = data;
 
-  let airportsTable;
+  let airportsSection;
   if (air_transport) {
-    const { airports, ...air_transport_rest } = air_transport;
-    airportsTable = generateAirportsTable(airports);
+    const { airports, national_system, ...air_transport_rest } = air_transport;
+    const airportsTable = generateAirportsTable(airports);
+    const nationalSystemList = generateNationalSystemList(national_system);
     rest.air_transport = air_transport_rest;
+
+    airportsSection = (
+      <>
+        {nationalSystemList}
+        {airportsTable}
+      </>
+    );
   }
 
   const isRestTreeNonEmpty = Object.keys(rest).length !== 0;
 
   return (
     <Container text>
-      {airportsTable}
+      {airportsSection}
       {isRestTreeNonEmpty && <JSONTree data={rest} theme={theme} />}
     </Container>
   );
